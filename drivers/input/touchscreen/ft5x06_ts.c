e2e3425390df81d84f3b47c79fff70386766a24f
Haruka <ichihara.rifa@gmail.com>
2016-03-30 17:02:57 +0700

    touchscreen: Resume When Dozing

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -1367,7 +1367,7 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK) {
+		if (*blank == FB_BLANK_UNBLANK || *blank == FB_BLANK_VSYNC_SUSPEND) { 
 			if (unblanked_once)
 				ft5x06_ts_resume(&ft5x06_data->client->dev);
 		} else if (*blank == FB_BLANK_POWERDOWN) {
