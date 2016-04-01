948d1424450adca6d94f4c5b168670b3eaf1c6d2
Haruka <ichihara.rifa@gmail.com>
2016-04-02 05:48:20 +0700

    uksm: fix error about conflicting types for 'try_to_unmap_ksm'

--- mm/uksm.c
+++ mm/uksm.c
@@ -4714,7 +4714,7 @@ out:
 	return referenced;
 }
 
-int try_to_unmap_ksm(struct page *page, enum ttu_flags flags)
+int try_to_unmap_ksm(struct page *page, enum ttu_flags flags, struct vm_area_struct *target_vma)
 {
 	struct stable_node *stable_node;
 	struct node_vma *node_vma;
