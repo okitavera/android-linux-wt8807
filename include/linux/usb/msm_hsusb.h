3957df23012ba55beed7d200a1db0f96c2c2f131
Haruka <ichihara.rifa@gmail.com>
2016-04-14 01:41:16 +0700

    msm_hsusb: turn down fastchg mA to 1100
    anticipate hardware issue on another wt8x047

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -104,13 +104,8 @@ enum msm_usb_phy_type {
 	SNPS_FEMTO_PHY,
 };
 
-#ifdef CONFIG_MACH_WT88047
 #define IDEV_CHG_MAX		1050
-#define IDEV_CHG_MAX_FAST	1500
-#else
-#define IDEV_CHG_MAX		1500
-#define IDEV_CHG_MAX_FAST	1500
-#endif
+#define IDEV_CHG_MAX_FAST	1100
 #define IDEV_CHG_MIN	500
 #define IUNIT		100
 
